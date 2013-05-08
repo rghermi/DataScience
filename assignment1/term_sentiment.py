44e784a8d7ca0c94fbb9ac09f77951a39505159a
rghermi <user@user-VirtualBox.(none)>
2013-05-08 15:15:49 -0700

    assignment 2

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,32 @@
 import sys
+import re
+import json
 
-def hw():
-    print 'Hello, world!'
+sent_file = open(sys.argv[1])
+tweet_file = open(sys.argv[2])
 
-def lines(fp):
-    print str(len(fp.readlines()))
+scores = {} # initialize an empty dictionary
+
+
+def writeDictionary():
+    for line in sent_file:
+  	term, score  = line.split("\t")  # The file is tab-delimited. 						"\t" means "tab character"
+  	scores[term] = int(score)  # Convert the score to an integer.
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+	writeDictionary()
+
+for fileLine in tweet_file:   #read the file line by line
+   sentiment = 0.0
+   tweet = json.loads(fileLine)
+
+   if tweet.has_key("text"):
+        for word in re.sub("[^\w]", " ", tweet["text"]).split(): 
+             if word.encode('utf-8') in scores.keys():
+		sentiment = sentiment + float(scores[word])
+                print sentiment 
+
 
 if __name__ == '__main__':
     main()
