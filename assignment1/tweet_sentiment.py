7d74ba24c2cc93a7c9fa3f7b6512d095a4539742
rghermi <user@user-VirtualBox.(none)>
2013-05-08 20:36:30 -0700

    no entra en el ultimo if

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -10,22 +10,29 @@ scores = {} # initialize an empty dictionary
 
 def writeDictionary():
     for line in sent_file:
-  	term, score  = line.split("\t")  # The file is tab-delimited. 						"\t" means "tab character"
-  	scores[term] = int(score)  # Convert the score to an integer.
+       term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+       scores[term] = int(score)  # Convert the score to an integer.
 
 
 def main():
-	writeDictionary()
+   writeDictionary()
+   #print scores.items()
 
-for fileLine in tweet_file:   #read the file line by line
+for tweetLine in tweet_file:   #read the file line by line
+   #looping each tweet in the file   
    sentiment = 0.0
-   tweet = json.loads(fileLine)
-
-   if tweet.has_key("text"):
-        for word in re.sub("[^\w]", " ", tweet["text"]).split(): 
-             if word.encode('utf-8') in scores.keys():
-		sentiment = sentiment + float(scores[word])
-                print sentiment 
+   tweet = json.loads(tweetLine)
+   
+   if tweet.has_key("text"):     # the text of the tweet
+      #print tweet["text"]   
+      for word in re.sub("[^\w]", " ", tweet["text"]).split():
+      #looping each word of the tweet 
+         print word
+         #if word.encode('utf-8') in scores.keys():
+         if scores.has_key(word.encode('utf-8')):
+            sentiment += float(scores[word])
+            print "ha entrado"
+      #print sentiment 
 
 
 if __name__ == '__main__':
